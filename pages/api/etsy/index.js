684fccedefa62e75174547a1aa77a0967a889c2e
Paula Hightower <eurolife@gmail.com>
2021-12-21 11:14:40 +0000

    Test etsy api

---
+++ pages/api/etsy/index.js
@@ -0,0 +1,54 @@
+import Cors from 'cors';
+import axios from 'axios';
+
+// Initializing the cors middleware
+const cors = Cors({
+  methods: ['GET', 'HEAD'],
+});
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
+
+async function handler(req, res) {
+  // Run the middleware
+  await runMiddleware(req, res, cors);
+
+  // Rest of the API logic
+  const resp = await getData();
+  res.json(resp);
+}
+
+async function getData() {
+  try {
+    let res = await axios({
+      // this pulls the colouring book section items
+      url: 'https://openapi.etsy.com/v2/shops/23419135/sections/34717543/listings/active?api_key=pdxsk5k85onbvfqxmutfn0km',
+      method: 'get',
+      timeout: 8000,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    if (res.status == 200) {
+      // test for status you want, etc
+      console.log(res.status);
+    }
+    // Don't forget to return something
+    return res.data;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+export default handler;
